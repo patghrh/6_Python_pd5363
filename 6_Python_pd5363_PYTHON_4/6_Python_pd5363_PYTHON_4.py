1b8b279c96a5ce9c87fbcf98ec7e4edfb3a3428b
Patrycja <pgardias1@gmail.com>
2026-05-01 22:13:39 +0200

    Refactor print statements for clarity

--- 6_Python_pd5363_PYTHON_4/6_Python_pd5363_PYTHON_4.py
+++ 6_Python_pd5363_PYTHON_4/6_Python_pd5363_PYTHON_4.py
@@ -3,8 +3,8 @@ dna_target = 'ATCGACGATGG'
 seq_length = 0.4 # Długość w kb
 print('Type of gen1: ', type(gen1), 'Type of dna_target: ', type(dna_target), 'Type of seq_length: ', type(seq_length))
 print('Gen: ', gen1)
-print('DNA target: ', dna_target, type(dna_target))
-print('Sequence length: ', type(seq_length), seq_length, 'kb')
+print('DNA target: ', dna_target)
+print('Sequence length: ', seq_length, 'kb')
 # Obliczanie procentu targetu w sekwencji
 print('% Target/Sequence: ', (len(dna_target) / (seq_length * 1000)) * 100, '%')
 
